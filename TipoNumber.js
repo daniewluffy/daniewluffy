f60bacfbf9ca46ab8f6186de1bb8f1448cc36ad8
Daniel Guimaraes <danielguimaraes224@gmail.com>
2021-09-15 16:40:04 -0300

    códigos var e conversões

--- TipoNumber.js
+++ TipoNumber.js
@@ -2,5 +2,9 @@ const primerironumero = 2.6;
 const segundoNumero = 3;
 
 const soma = primerironumero + segundoNumero;
-
 console.log(soma)
+
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+
---
+++ Tipos.js
@@ -0,0 +1,11 @@
+let numero = 3;
+let texto = "alura";
+let booleano = true;
+let minhavar;
+let varNull = null;
+
+console.log(typeof numero)
+console.log(typeof texto)
+console.log(typeof booleano)
+console.log(typeof minhavar)
+console.log(typeof varNull)
